a1ee10d0a456ae3ea1803d6d95912e1c731bcc76
VadimChernik <chernik.vadim@gmail.com>
2025-04-16 21:07:59 +0000

    upd dbt

--- dbt/bikeshare_trips/models/core/fct_trips.sql
+++ dbt/bikeshare_trips/models/core/fct_trips.sql
@@ -1,4 +1,11 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "start_date",
+      "data_type": "date",
+      "granularity": "day"
+    }
+)}}
 
 with tripdata as (
     select 
